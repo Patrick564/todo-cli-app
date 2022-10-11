31dfacbcdc5ea5d3d5aa84067c86e6b8113ac729
Patrick Vilchez <pvilchez794@gmail.com>
2022-10-11 13:05:30 -0500

    Tests for list command refactor to table test case

--- pkg/cmd/list/list_test.go
+++ pkg/cmd/list/list_test.go
@@ -8,26 +8,34 @@ import (
 
 // TODO: add table test for list all, completed, and pending
 func TestList(t *testing.T) {
-	fs := fstest.MapFS{
-		"all.md": {Data: []byte(`1. fake task example
-5. second fake task
-13. two digits id fake task
-`)},
+	cases := []struct {
+		name string
+		file fstest.MapFS
+		want []Task
+	}{
+		{
+			name: "all.md file with 3 tasks",
+			file: fstest.MapFS{
+				"all.md": {Data: []byte("1. fake task example\n5. second fake task\n13. two digits id fake task")},
+			},
+			want: []Task{
+				{Id: "1", Content: "fake task example"},
+				{Id: "5", Content: "second fake task"},
+				{Id: "13", Content: "two digits id fake task"},
+			},
+		},
 	}
 
-	task, err := readTasksFromFile(fs)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got, err := readTasksFromFile(c.file)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	got := task
-	want := []Task{
-		{Id: "1", Content: "fake task example"},
-		{Id: "5", Content: "second fake task"},
-		{Id: "13", Content: "two digits id fake task"},
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("want %s but got %s", want, got)
+			if !reflect.DeepEqual(got, c.want) {
+				t.Errorf("want %s but got %s", c.want, got)
+			}
+		})
 	}
 }
